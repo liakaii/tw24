103ce87d0cda8037f82a4fe00d5fec16b5ac77db
liakaii <oneshot630@gmail.com>
2024-03-22 16:17:21 +0200

    My commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ function App() {
     <div>
     <Menuu />
     </div>
-      <h1>Vite + React</h1>
+      <h1>Новости</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          прибавь цифру {count}
         </button>
         <p>
           Ну<code>...</code> типа привет
